4c4ea7253cf9d55d31b6fc639c59865f52980a2f
DoanNguyen2709 <dn938901ahigi@gmail.com>
2024-01-24 21:05:15 +0700

    backend

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "doan@gmail2.com",
+        password: "123456",
+        firstName: "Doan",
+        lastName: "Van",
+        address: "VN",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
